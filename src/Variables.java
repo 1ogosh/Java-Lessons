430502ebb486f0d0f2dc7ee35bfb50b2b65f0592
Andrew Salmanov <ads739912@gmail.com>
2024-02-01 16:20:33 +0200

    Public

---
+++ src/Variables.java
@@ -0,0 +1,13 @@
+public class Variables {
+    public static void main(String[] args) {
+        int myInt = 557;
+            short myShort = 3266;
+                long myLong = 2626262;
+                double myDouble = 235.25;
+                float myFloat = 2365.4f;
+                char c = 'a';
+                boolean b = true;
+
+        System.out.println(myInt);
+    }
+}
